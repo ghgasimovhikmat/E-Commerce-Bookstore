3ee308f507d1a929e06161285d2440863c3168a1
ghgasimovhikmat <gh.gasimovhikmat@gmail.com>
2026-07-13 17:16:38 +0300

    handled image in post endpoint and edit in Upsert Part and Edit in action

--- BulkyBookWeb/Areas/Customer/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/ProductController.cs
@@ -16,18 +16,19 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
         private readonly IProductService _productService;
         private readonly ICatagoryService _catagoryService;
 
-        public ProductController(IProductService productService, ICatagoryService catagoryService)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IProductService productService, ICatagoryService catagoryService, IWebHostEnvironment webHostEnvironment)
         {
             _productService = productService;
             _catagoryService = catagoryService;
-
+            _webHostEnvironment = webHostEnvironment;
         }
         public async Task<IActionResult> Index()
         {
                  return View();
         }
        
-        public async Task<IActionResult> Upsert()
+        public async Task<IActionResult> Upsert(int? id)
         {
             var categories = await _catagoryService.GetAllCategoriesAsync();
             ProductVM productVM = new ProductVM()
@@ -39,24 +40,76 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                     Value = c.Id.ToString()
                 })
             };
+            if(id==null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product= await _productService.GetProductByIdAsync(id.Value);
+                return View(productVM);
+            }
            
-                      return View(productVM);
+         
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         [ActionName("Upsert")]
-        public async Task<IActionResult> UpsertPOST(Product product,IFormFile? file)
+        public async Task<IActionResult> UpsertPOST(ProductVM productVM, IFormFile? file)
         {
-
 
             if (ModelState.IsValid)
             {
-              //  await  _productService.CreateProductAsync(product);
-               
+
+
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine("images", "products");
+                    string finalPath = Path.Combine(wwwRootPath, productPath);
+
+
+                    if (!Directory.Exists(finalPath))
+                        Directory.CreateDirectory(finalPath);
+
+                    //save the new image
+                    using (var fileStream = new FileStream(Path.Combine(finalPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                   productVM.Product.ImageUrl = Path.Combine(@"\", productPath, fileName).Replace("\\", "/");
+                  
+                }
+                if (productVM.Product.Id == null || productVM.Product.Id == 0)
+                {
+                    await _productService.CreateProductAsync(productVM.Product);
+                }
+                else
+                {
+                    await _productService.UpdateProductAsync(productVM.Product);
+
+                }
+
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                var categories = await _catagoryService.GetAllCategoriesAsync();
+                productVM = new ProductVM()
+                {
+                    Product = new Product(),
+                    CategoryList = categories.Select(c => new SelectListItem
+                    {
+                        Text = c.Name,
+                        Value = c.Id.ToString()
+                    })
+                };
+                return View(productVM);
+            }
+           
 
         }
          public async Task<IActionResult> Delete(int? Id)
